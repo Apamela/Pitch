e01bfc1cc313dd4cf264a40b6ba5c7b7ea312c2a
Apamela <pamelab.desire@gmail.com>
2019-09-23 11:28:17 +0200

    add new folder

---
+++ app/main/views.py
@@ -0,0 +1,91 @@
+from flask import render_template,request,redirect,url_for,abort
+from . import main
+# from ..request import get_movies,get_movie,search_movie
+from .forms import ReviewForm,UpdateProfile
+from ..models import Review,User,PhotoProfile
+from flask_login import login_required,current_user
+from .. import db,photos
+
+# Views
+@main.route('/')
+def index():
+
+    '''
+    View root page function that returns the index page and its data
+    '''
+
+@main.route('/pitch/<int:id>')
+def movie(id):
+
+    '''
+    View pitch page function that returns the pitch details page and its data
+    '''
+    pitch = get_pitch(id)
+    title = f'{pitch.title}'
+    reviews = Review.get_reviews(pitch.id)
+
+    return render_template('pitch.html',title = title,pitch = pitch,reviews = reviews)
+
+
+
+@main.route('/pitch/review/new/<int:id>', methods = ['GET','POST'])
+@login_required
+def new_review(id):
+
+    form = ReviewForm()
+    pitch = get_pitch(id)
+
+    if form.validate_on_submit():
+        title = form.title.data
+        review = form.review.data
+        #update review instance
+        new_review = Review(pitch.id,title,pitch.poster,pitch_review=review,user=current_user)
+        #save review method
+        new_review.save_review()
+        return redirect(url_for('.pitch',id = pitch.id ))
+
+    title = f'{pitch.title} review'
+    return render_template('new_review.html',title = title, review_form=form, pitch=pitch)
+
+@main.route('/user/<name>')
+@login_required
+def profile(name):
+    user = User.query.filter_by(username = name).first()
+
+    if user is None:
+        abort(404)
+
+    return render_template("profile/profile.html", user = user)
+
+
+@main.route('/user/<name>/update',methods = ['GET','POST'])
+@login_required
+def update_profile(name):
+    user = User.query.filter_by(username = name).first()
+    if user is None:
+        abort(404)
+
+    form = UpdateProfile()
+
+    if form.validate_on_submit():
+
+        user.bio = form.bio.data
+
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for('.profile',name=user.username))
+
+    return render_template('profile/update.html',form =form)
+
+
+@main.route('/user/<name>/update/pic',methods= ['POST'])
+@login_required
+def update_pic(name):
+    user = User.query.filter_by(username = name).first()
+    if 'photo' in request.files:
+        filename = photos.save(request.files['photo'])
+        path = f'photos/{filename}'
+        user.profile_pic_path = path
+        db.session.commit()
+    return redirect(url_for('main.profile',name=name))
