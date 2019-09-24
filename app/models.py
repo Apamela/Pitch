62d6065e8e2825d31b03be9006759de09551a53a
Apamela <pamelab.desire@gmail.com>
2019-09-24 09:53:32 +0200

    add new folder

--- app/models.py
+++ app/models.py
@@ -19,22 +19,22 @@ class Pitch:
         self.poster = "https://image.tmdb.org/t/p/w500/" + poster
         self.vote_average = vote_average
         self.vote_count = vote_count
-class User(,UserMixin,db.Model):
+class User(UserMixin,db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer,primary_key = True)
     username = db.Column(db.String(255))
     role_id=db.Column(db.Integer,db.ForeignKey('roles.id'))
     pass_secure = db.Column(db.String(255))
-     @property
-        def password(self):
+    @property
+    def password(self):
             raise AttributeError('You cannot read the password attribute')
 
-        @password.setter
-        def password(self, password):
+    @password.setter
+    def password(self, password):
             self.pass_secure = generate_password_hash(password)
 
 
-        def verify_password(self,password):
+    def verify_password(self,password):
             return check_password_hash(self.pass_secure,password)
     def __repr__(self):
         return f'User {self.username}'
