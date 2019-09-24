5a2a467ec5501d74b4cb413e467f890dd39def20
Apamela <pamelab.desire@gmail.com>
2019-09-24 10:28:16 +0200

    add new folder

--- app/__init__.py
+++ app/__init__.py
@@ -5,36 +5,35 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 
 login_manager = LoginManager()
+
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
 
 def create_app(config_name):
-    #....
-    #Initializing Flask Extensions
-    bootstrap.init_app(app)
-    db.init_app(app)
-    login_manager.init_app(app)
-bootstrap = Bootstrap()
-db = SQLAlchemy()
-
-def create_app(config_name):
 
     app = Flask(__name__)
+
 
     # Creating the app configurations
 
     app.config.from_object(config_options[config_name])
     config_options[config_name].init_app(app)
 
-    # Initializing flask extensions
+    #Initializing Flask Extensions
     bootstrap.init_app(app)
     db.init_app(app)
+    login_manager.init_app(app)
+# bootstrap = Bootstrap()
+# db = SQLAlchemy()
+
 
 
     # Registering the blueprint
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint,url_prefix='/auth')
     # setting config
     from .requests import configure_request
     configure_request(app)
---
+++ config.py
@@ -0,0 +1,41 @@
+import os
+
+class Config:
+
+    MOVIE_API_BASE_URL ='https://api.themoviedb.org/3/movie/{}?api_key={}'
+    MOVIE_API_KEY ='ff73536b7f0e63d45fe4f61ecf8eb703'
+    SECRET_KEY ='a rondom string'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://wecode:pamela@localhost/watchlist'
+    UPLOADED_PHOTOS_DEST='app/static/photos'
+    
+    
+    #  email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+
+class ProdConfig(Config):
+    pass
+
+
+class DevConfig(Config):
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+
+}
+class TestConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://james:password@localhost/watchlist_test'
+
+class DevConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://james:password@localhost/watchlist'
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig,
+'test':TestConfig
+}
